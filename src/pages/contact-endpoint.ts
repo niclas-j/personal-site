c63ad41e68dc323bb0e4a1ac11a9c48abcd1cff9
Niclas Jost <35239311+niclas-j@users.noreply.github.com>
2023-04-12 21:39:42 +0200

    telegram

---
+++ src/pages/contact-endpoint.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from "astro";
+import TelegramBot from "node-telegram-bot-api";
+
+const token = import.meta.env.TELEGRAM_BOT_TOKEN;
+const chatId = import.meta.env.TELEGRAM_CHAT_ID;
+
+const bot = new TelegramBot(token, { polling: false });
+
+export const post: APIRoute = async function post({ request, redirect }) {
+  const data = await request.formData();
+
+  bot.sendMessage(chatId, `Telefonnummer: ${data.get("phone")}, Email: ${data.get("email")}, Message: ${data.get("message")}`);
+
+  return redirect("/contact?success=true");
+};

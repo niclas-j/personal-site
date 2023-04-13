a6d104ce818d0086926af45f399cf6e041e5ef02
Niclas Jost <35239311+niclas-j@users.noreply.github.com>
2023-04-13 11:22:55 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   experimental: {
-    assets: true
+    assets: true,
   },
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true,
+  }),
 });

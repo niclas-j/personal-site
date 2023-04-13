e89cba1c973ef0a93a4edcd3dbb6019e8ffd7d76
Niclas Jost <35239311+niclas-j@users.noreply.github.com>
2023-04-13 11:24:04 +0200

    serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
